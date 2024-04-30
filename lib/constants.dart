1f1db72b63278bd1f010f5d0ac88971709d83a79
dell <elocklymuhamed@gmail.com>
2024-04-30 20:00:00 +0200

    Add Home View

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF100B20);
+const kTransationDuration = Duration(milliseconds: 250);
--- lib/features/splash/presentaion/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentaion/views/widgets/splash_view_body.dart
@@ -1,8 +1,11 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:bookly_app/features/home/presentaion/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentaion/views/widgets/sliding_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody();
@@ -19,6 +22,26 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initState() {
     // TODO: implement initState
     super.initState();
+    initAnimation();
+    navigateToHomeView();
+  }
+
+  void navigateToHomeView() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Get.to(
+          () {
+            return HomeView();
+          },
+          transition: Transition.fade,
+          duration: kTransationDuration,
+        );
+      },
+    );
+  }
+
+  void initAnimation() {
     animationController = AnimationController(
       vsync: this,
       duration: Duration(seconds: 2),
