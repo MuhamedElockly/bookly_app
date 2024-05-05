6873f9502b5749c0105af81e0e9e02c7c12c6f4a
dell <elocklymuhamed@gmail.com>
2024-05-06 01:51:46 +0200

    add title to detail page

--- lib/features/home/presentaion/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentaion/views/widgets/book_details_view_body.dart
@@ -1,17 +1,42 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentaion/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentaion/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly_app/features/home/presentaion/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          
           CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            child: FeaturedListViewItem(),
+          ),
+          SizedBox(
+            height: 43,
+          ),
+          Text(
+            "The Jungle Book",
+            style: Styles.textStyle30,
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              "Rydialk Klibing",
+              style: Styles.textStyle14.copyWith(
+                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            ),
+          ),
         ],
       ),
     );
