ef54d1428c9ec90e2bf77992c93969161e264cdb
dell <elocklymuhamed@gmail.com>
2024-05-14 01:52:33 +0200

    last update

--- lib/core/widgets/custom_loading_indicator.dart
+++ lib/core/widgets/custom_loading_indicator.dart
@@ -2,24 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
 class CustomLoadingIndicator extends StatelessWidget {
-   CustomLoadingIndicator();
+  CustomLoadingIndicator();
   @override
   Widget build(BuildContext context) {
-    return Shimmer.fromColors(
-      baseColor: Colors.grey.shade300,
-      highlightColor: Colors.grey.shade100,
-      enabled: true,
-      child:  SingleChildScrollView(
-        physics: NeverScrollableScrollPhysics(),
-        child:  Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Container(
-              color: Colors.grey.shade300,
-            ),
-          ],
-        ),
+    List<Color> colors = [Colors.yellow, Colors.grey];
+    return Shimmer(
+      child: Container(),
+      gradient: LinearGradient(
+        colors: colors,
       ),
     );
   }
